a9dfde1d4576ea6f9583dcfb21239804c8b70e32
Vinicius040496 <vinicius.almeida2011@gmail.com>
2024-05-07 13:32:37 -0300

    Adicionar arquivos de projeto.

---
+++ Logica de programação 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Logica_de_programação_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Escreva um algoritmo para ler um valor, e escreva na tela o seu antecessor.
+
+            Console.WriteLine(" Escreva um numero: ");
+            int valor;
+            valor = int.Parse(Console.ReadLine());
+            int antecessor = valor - 1;
+            Console.WriteLine(" O valor digitado é " + valor + (" Seu antecessor é ") + antecessor);
+            Console.ReadLine();
+
+        }
+    }
+}
